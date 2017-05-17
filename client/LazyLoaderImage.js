2cb0eb95fa9720c38008f2ce92107663deeaa0db
Anwesh43 <anweshthecool0@gmail.com>
2017-05-17 13:52:18 +0530

    Using creating and using circle loader

--- client/LazyLoaderImage.js
+++ client/LazyLoaderImage.js
@@ -6,6 +6,7 @@ class LazyLoaderImage {
         this.y = y
         this.w = w
         this.h = h
+        this.circleLoader = new CircleLoader()
     }
     create() {
         this.canvas = document.createElement('canvas')
@@ -20,11 +21,35 @@ class LazyLoaderImage {
     startRenderLoop() {
         setInterval(()=>{
             this.render()
-        },100)
+        },50)
     }
     render() {
         this.context.clearRect(0,0,this.w,this.h)
         this.context.fillStyle = "#9E9E9E"
         this.context.fillRect(0,0,this.w,this.h)
+        this.circleLoaded.draw(context,this.w/2,this.h/2,this.w/6)
+        this.circleLoaded.update()
+    }
+}
+class CircleLoader  {
+    constructor() {
+        this.deg = 0
+    }
+    draw(context,x,y,r) {
+        context.strokeStyle = '#BDBDBD'
+        context.beginPath()
+        for(var i=this.deg;i<this.deg+60;i++) {
+            const point = {x:x+r*Math.cos(i*Math.PI/180),y:y+r*Math.sin(i*Math.PI/180)}
+            if(i == this.deg) {
+                context.moveTo(point.x,point.y)
+            }
+            else {
+                context.lineTo(point.x,point.y)
+            }
+        }
+        context.stroke()
+    }
+    update() {
+        this.deg += 10
     }
 }
